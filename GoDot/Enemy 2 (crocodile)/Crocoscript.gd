cb3c74d32cae3022e94145356ed7f74fe3a51677
violaaaR <viola.rosendahl@gmail.com>
2024-09-17 14:21:52 +0200

    Merge branch 'main' of https://github.com/violaaaR/Vaerkstedsuge.github-brug-denne

---
+++ GoDot/Enemy 2 (crocodile)/Crocoscript.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+const SPEED = 60
+var direction = 1
+
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+@onready var animated_sprite = $AnimatedSprite2D
+
+@onready var edge_detector_right = $edgeDetectorRight
+@onready var edge_detector_left = $edgeDetectorLeft
+
+
+func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = false
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = true
+
+	position.x += direction * SPEED * delta 
+
+
+
+
+func _on_area_2d_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	pass # Replace with function body.

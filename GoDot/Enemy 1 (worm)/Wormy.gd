6d7066a8eeb93f75b966a1f7ea3564d9a989d138
Manu-Boss <bansal.manasvi21@gmail.com>
2024-09-17 17:59:45 +0200

    Combat (worm and Player)
    De gør skade på hinanden!!! LETS GOOO

--- GoDot/Enemy 1 (worm)/Wormy.gd
+++ GoDot/Enemy 1 (worm)/Wormy.gd
@@ -10,7 +10,13 @@ var direction = 1
 @onready var edge_right = $edgeRight
 @onready var edge_left = $edgeLeft
 
+var health = 1
+var player_inattack_zone = false
+var enemy_attack_cooldown = true
 
+func _physics_process(delta):
+	deal_with_damage()
+	enemy_attack()
 
 func _process(delta):
 	if ray_cast_right.is_colliding() or edge_right.is_colliding() == false:
@@ -22,4 +28,29 @@ func _process(delta):
 
 	position.x += direction * SPEED * delta 
 	
+func enemy():
+	pass 
 
+func _on_worm_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+func _on_worm_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_inattack_zone = false
+
+func enemy_attack():
+	if player_inattack_zone == true: 
+		GameManager.enemy_attack = true 
+		enemy_attack_cooldown = false 
+		await get_tree().create_timer(0.5).timeout 
+		enemy_attack_cooldown = true
+		GameManager.enemy_attack = false
+
+func deal_with_damage():
+	if player_inattack_zone and GameManager.player_current_attack == true:
+		health = health - 1
+		await get_tree().create_timer(1).timeout
+		print("enemy health - 1")
+		if health <= 0: 
+			self.queue_free()

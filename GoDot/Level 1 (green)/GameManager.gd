79a220abeca785bc0b00c13a12b3c7fdad6a4391
violaaaR <viola.rosendahl@gmail.com>
2024-09-19 01:07:36 +0200

    labels (score + points i level 1)

--- GoDot/Level 1 (green)/GameManager.gd
+++ GoDot/Level 1 (green)/GameManager.gd
@@ -2,24 +2,26 @@ extends Node
 
 var score = 0 
 var addscore = 0
-
-static var totalscore = 0 
+var totalscore = 0 
 
 @onready var score_label = $CanvasLayer/ScoreLabel
+@onready var total_score_label = $CanvasLayer/TotalScoreLabel
 
 func add_point():
 	score += 1
-	score_label.text = str(score) + " / 10"
+	score_label.text = str(score).pad_zeros(2) + "/10"
 
 func add_extra_point():
 	if GameManager.enemy_type == "Wormy":
-		addscore += 1
+		addscore += 5
 	elif GameManager.enemy_type == "Bear":
-		addscore += 2
+		addscore += 10
 	elif GameManager.enemy_type == "Crocodile":
-		addscore += 3
+		addscore += 20
 	#score_label.text = "Score: " + str(score) + " / 10" + " + " + str(addscore)
 
 func _process(delta): #TOTAL POINTS
 	totalscore = score + addscore
 	GameManager.current_level_score = totalscore
+	
+	total_score_label.text = "POINTS:" + str(totalscore).pad_zeros(2)

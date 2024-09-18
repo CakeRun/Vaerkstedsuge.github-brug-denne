ec6d83924b99176c92ed9568dd71080a2d1202f3
Manu-Boss <bansal.manasvi21@gmail.com>
2024-09-18 10:05:34 +0200

    Så BURDE alle enemies kunne angribe og tage skade

--- GoDot/Enemy 3 (bear)/enemy_3_script.gd
+++ GoDot/Enemy 3 (bear)/enemy_3_script.gd
@@ -3,6 +3,12 @@ extends CharacterBody2D
 @onready var bear = $AnimatedSprite2D
 @onready var player = $"../CharacterBody2D" #muligvis ikke vores player
 
+var health = 3
+var player_inattack_zone = false
+var enemy_attack_cooldown = true
+
+@onready var game_manager = %GameManager
+
 var speed = 200
 var bear_status = "sleeping"
 
@@ -72,7 +78,31 @@ func _on_wakeup_area_body_exited(player):
 	bear_status = "fall_asleep"
 
 func _on_attack_area_body_entered(player):
-	bear_status = "attack"
+	if player.has_method("player"):
+		player_inattack_zone = true
 
 func _on_attack_area_body_exited(player):
-	bear_status = "chase"
+	if player.has_method("player"):
+		player_inattack_zone = false
+
+func enemy_attack():
+	if player_inattack_zone and enemy_attack_cooldown == true:
+		GameManager.enemy_type = "Bear" 
+		GameManager.enemy_attack = true 
+		bear_status = "attack"
+		enemy_attack_cooldown = false 
+		await get_tree().create_timer(0.02).timeout #til at sikre, at vi dør ikke konstant
+		GameManager.enemy_attack = false
+		await get_tree().create_timer(1).timeout
+		enemy_attack_cooldown = true
+	elif player_inattack_zone == false:
+		bear_status = "chase"
+
+func deal_with_damage():
+	if player_inattack_zone and GameManager.player_current_attack == true:
+		health = health - 1
+		await get_tree().create_timer(0.5).timeout #her dør den ikke med det samme
+		print("enemy health - 1")
+		if health <= 0: 
+			game_manager.add_extra_point()
+			self.queue_free()

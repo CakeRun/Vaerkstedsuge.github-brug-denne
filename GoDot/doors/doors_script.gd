efabb27330f121a0290b0b202d6b877b6ad8344c
violaaaR <viola.rosendahl@gmail.com>
2024-09-18 22:13:42 +0200

    DØRER FUNGERE ALLE

--- GoDot/doors/doors_script.gd
+++ GoDot/doors/doors_script.gd
@@ -4,9 +4,10 @@ extends Node
 @onready var teleport_audio_green = $"../teleport_audio_green"
 @onready var teleport_audio_orange = $"../teleport_audio_orange"
 @onready var teleport_audio_blue = $"../teleport_audio_blue"
-
+@onready var player = $"../Player"
 
 static var levels_completed = 0
+var current_scene = ""
 
 #LEVEL 1
 static var entered_green_main = false
@@ -21,6 +22,7 @@ static var entered_blue_main = false
 static var entered_blue_level3 = false
 
 
+
 func _process(delta):
 	#print(levels_completed)
 	
@@ -33,33 +35,6 @@ func _process(delta):
 			#teleport_audio_main.play()
 			#await get_tree().create_timer(1).timeout
 			get_tree().change_scene_to_file("res://Level 1 (green)/Level 1.tscn")
-			entered_green_main = false
-	
-	if GameManager.current_level_score >= 3 and Input.is_action_just_pressed("enter"):
-		#green
-		if entered_green_level1 == true:
-			print("going to main")
-			#teleport_audio_green.play()
-			#await get_tree().create_timer(1).timeout
-			get_tree().change_scene_to_file("res://main scene (start)/main.tscn")
-			
-			if levels_completed == 0: #sikrer at det kun updaterer det én gang
-				levels_completed = 1
-				print(levels_completed)
-				entered_green_level1 = false
-		
-		#orange
-		if entered_orange_level2 == true:
-			print("going to main")
-			#teleport_audio_green.play()
-			#await get_tree().create_timer(1).timeout
-			get_tree().change_scene_to_file("res://main scene (start)/main.tscn")
-			
-			if levels_completed == 1: #sikrer at det kun updaterer det én gang
-				levels_completed = 2
-				print(levels_completed)
-				entered_green_level1 = false
-
 
 	#### LEVEL 2 ####
 	if entered_orange_main == true and Input.is_action_just_pressed("enter"):
@@ -70,17 +45,54 @@ func _process(delta):
 			#teleport_audio_main.play()
 			#await get_tree().create_timer(1).timeout
 			get_tree().change_scene_to_file("res://level 2 (fall)/Level 2.tscn")
-			entered_orange_main = false
 	
 
+	#### LEVEL 3 ####
+	if entered_blue_main == true and Input.is_action_just_pressed("enter") and levels_completed == 2:
+		print(levels_completed)
+		
+		if levels_completed == 2:
+			print("entering level 3")
+			#teleport_audio_main.play()
+			#await get_tree().create_timer(1).timeout
+			get_tree().change_scene_to_file("res://Level 3 (winter)/level_3.tscn")
+	
+	
+	#### Leaving a level ####
+	if GameManager.current_level_score >= 0 and Input.is_action_just_pressed("enter"):
+		#green
+		if entered_green_level1 == true:
+			print("going to main")
+			teleport_audio_green.play()
+	
+			await get_tree().create_timer(1).timeout
+			get_tree().change_scene_to_file("res://main scene (start)/main.tscn")
 
-	##### LEVEL 3 ####
-	#if entered_blue_main == true and Input.is_action_just_pressed("enter") and levels_completed == 2:
-		#pass
-	#
-	#if GameManager.current_level_score >= 1:
-		#pass
+			if levels_completed == 0: #sikrer at det kun updaterer det én gang
+				levels_completed = 1
+				print(levels_completed)
 
+		#orange
+		if entered_orange_level2 == true:
+			print("going to main")
+			#teleport_audio_green.play()
+			#await get_tree().create_timer(1).timeout
+			get_tree().change_scene_to_file("res://main scene (start)/main.tscn")
+			
+			if levels_completed == 1: #sikrer at det kun updaterer det én gang
+				levels_completed = 2
+				print(levels_completed)
+		
+		#blue
+		if entered_blue_level3 == true:
+			print("going to main end")
+			#teleport_audio_green.play()
+			#await get_tree().create_timer(1).timeout
+			get_tree().change_scene_to_file("res://end scene/end.tscn")
+			
+			if levels_completed == 2: #sikrer at det kun updaterer det én gang
+				levels_completed = 3
+				print(levels_completed)
 
 #### LEVEL 1 ####
 #green door - main
@@ -112,3 +124,18 @@ func _on_area_2d_orange_level_2_body_entered(body):
 	entered_orange_level2 = true
 func _on_area_2d_orange_level_2_body_exited(body):
 	entered_orange_level2 = false
+
+
+#### LEVEL 3 ####
+#blue door - main
+func _on_area_2d_blue_main_body_entered(body):
+	if levels_completed == 2:
+		entered_blue_main = true
+func _on_area_2d_blue_main_body_exited(body):
+	entered_blue_main = false
+
+#blue door - level3
+func _on_area_2d_blue_level_3_body_entered(body):
+	entered_blue_level3 = true
+func _on_area_2d_blue_level_3_body_exited(body):
+	entered_blue_level3 = false

585106ab53a39d811ed6437ce3f7d7ff3145631a
violaaaR <viola.rosendahl@gmail.com>
2024-09-18 11:26:50 +0200

    doors and stuff (point før du kan gå ind - virker ikke endnu)

--- GoDot/Level 1 (green)/GameManager.gd
+++ GoDot/Level 1 (green)/GameManager.gd
@@ -10,6 +10,7 @@ var totalscore = 0
 func add_point():
 	score += 1
 	score_label.text = "Score: " + str(score) + " / 10"
+	print("you collected one thing")
 
 func add_extra_point():
 	if GameManager.enemy_type == "Wormy":
@@ -20,6 +21,6 @@ func add_extra_point():
 		addscore += 3
 	score_label.text = "Score: " + str(score) + " / 10" + " + " + str(addscore)
 
-func total_points():
+func total_points(delta):
 	totalscore = score + addscore
 	GameManager.current_level_score = totalscore
--- GoDot/global.gd
+++ GoDot/global.gd
@@ -12,3 +12,4 @@ var total_score = 0
 
 func game_score():
 	total_score = total_score + current_level_score
+	print(total_score)

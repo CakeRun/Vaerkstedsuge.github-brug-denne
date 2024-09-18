dab681664f57039601078c96b5ec6977102e3d2c
violaaaR <viola.rosendahl@gmail.com>
2024-09-18 13:07:16 +0200

    enemy3 fungerer!!! (animation)

--- GoDot/Enemy 3 (bear)/enemy_3_script.gd
+++ GoDot/Enemy 3 (bear)/enemy_3_script.gd
@@ -3,16 +3,8 @@ extends CharacterBody2D
 @onready var bear = $AnimatedSprite2D
 @onready var player = %Player
 
-
-var health = 3
-var player_inattack_zone = false
-var enemy_attack_cooldown = true
-
-@onready var game_manager = %GameManager
-
 var speed = 200
 var bear_status = "sleeping"
-
 
 func _physics_process(delta):
 	#collision boy yderst 
@@ -57,57 +49,28 @@ func _physics_process(delta):
 	
 
 	elif bear_status == "attack":
-		
 		var t = randf_range(0,1)
-		
-		if t<=0.05 : #2% change for at der angribes
-			bear.play("attacking")
+		if t<=0.05 : #5% change for at der angribes
+			bear.play("attack")
 			await get_tree().create_timer(1).timeout
 
-func _on_detection_area_body_entered(body):
-	if bear_status=="" and body.has_method("player"):
+func _on_detection_area_body_entered(player):
+	if bear_status=="":
 		bear_status = "chase"
 
-func _on_detection_area_body_exited(body):
-	if body.has_method("player"):
+func _on_detection_area_body_exited(player):
 		bear_status = ""
 		bear.play("idle")
 	
-func _on_wakeup_area_body_entered(body):
-	if bear_status == "sleeping" and body.has_method("player"):
+func _on_wakeup_area_body_entered(player):
+	if bear_status == "sleeping":
 		bear_status = "wakeup"
 
-func _on_wakeup_area_body_exited(body):
-	if body.has_method("player"):
-		bear_status = "fall_asleep"
+func _on_wakeup_area_body_exited(player):
+	bear_status = "fall_asleep"
 
-func _on_attack_area_body_entered(body):
-	if body.has_method("player"):
-		player_inattack_zone = true
+func _on_attack_area_body_entered(player):
+	bear_status = "attack"
 
-func _on_attack_area_body_exited(body):
-	if body.has_method("player"):
-		player_inattack_zone = false
-
-func enemy_attack():
-	if player_inattack_zone and enemy_attack_cooldown == true:
-		GameManager.enemy_type = "Bear" 
-		GameManager.enemy_attack = true 
-		bear_status = "attack"
-		enemy_attack_cooldown = false 
-		await get_tree().create_timer(0.02).timeout #til at sikre, at vi dør ikke konstant
-		GameManager.enemy_attack = false
-		await get_tree().create_timer(1).timeout
-		enemy_attack_cooldown = true
-	elif player_inattack_zone == false:
-		bear_status = "chase"
-
-func deal_with_damage():
-	if player_inattack_zone and GameManager.player_current_attack == true:
-		health = health - 1
-		await get_tree().create_timer(0.5).timeout #her dør den ikke med det samme
-		print("enemy health - 1")
-		if health <= 0: 
-			GameManager.enemy_type = "Bear"
-			game_manager.add_extra_point()
-			self.queue_free()
+func _on_attack_area_body_exited(player):
+	bear_status = "chase"

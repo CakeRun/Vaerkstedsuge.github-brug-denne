5c9e0a85527c61aa66637a5fa1aa3530ef569752
Sn0wyM30w <anesophiesonne@gmail.com>
2024-09-19 21:59:22 +0200

    tutorial scene platform + txt ting

--- GoDot/main_scenes/tutroial_scene/MainTutorialScene.gd
+++ GoDot/main_scenes/tutroial_scene/MainTutorialScene.gd
@@ -6,6 +6,8 @@ extends Node2D
 @onready var label_mission_2 = $txt/Label_mission2
 @onready var timer = $Timer
 @onready var block = $Doors/Block
+@onready var arrow_1 = $arrow1
+@onready var arrow_2 = $arrow2
 
 var BlockDoor = true
 # Called when the node enters the scene tree for the first time.
@@ -14,7 +16,8 @@ func _ready():
 	label_jump.visible = false
 	label_mission.visible = false
 	label_mission_2.visible = false
-
+	arrow_1.visible = false
+	arrow_2.visible = false
 
 #E to door
 func _on_area_2d_body_entered(body):
@@ -24,17 +27,24 @@ func _on_area_2d_body_exited(body):
 #space to jump
 func _on_jump_body_entered(body):
 	label_jump.visible = true
+	get_tree().create_timer(0.7)
+	timer.start()
+	await timer.timeout
+	arrow_1.visible = true
 func _on_jump_body_exited(body):
-	label_jump.visible = false
+	arrow_1.visible = false
 #mission
 func _on_mission_body_entered(body):
 	label_mission.visible = true
-	timer.start()
+	timer.start(0.4)
 	if BlockDoor == true:
 		block.queue_free()
 		BlockDoor = false
 	await timer.timeout
 	label_mission_2.visible = true
+	arrow_2.visible = true
+	#lav noget med pile
 func _on_mission_body_exited(body):
-	label_mission.visible = false
-	label_mission_2.visible = false
+		label_mission.visible = false
+		label_mission_2.visible = false
+		arrow_2.visible = false

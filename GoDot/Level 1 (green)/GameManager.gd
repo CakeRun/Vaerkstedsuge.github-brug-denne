a3f11a6d970774a5005ef2d0c4170c3b55471eda
Manu-Boss <bansal.manasvi21@gmail.com>
2024-09-19 09:51:31 +0200

    Reapply "Merge branch 'main' of https://github.com/violaaaR/Vaerkstedsuge.github-brug-denne"
    This reverts commit 4f5552db3d3380ef521acf142059a80ea9282c79.

--- GoDot/Level 1 (green)/GameManager.gd
+++ GoDot/Level 1 (green)/GameManager.gd
@@ -2,14 +2,13 @@ extends Node
 
 var score = 0 
 var addscore = 0
-@export var totalscore = 0 
 
+@export var totalscore = 0 
 @onready var score_label = $CanvasLayer/ScoreLabel
 @onready var total_score_label = $CanvasLayer/TotalScoreLabel
 
 func add_point():
 	score += 1
-	score_label.text = str(score).pad_zeros(2) + "/10"
 
 func add_extra_point():
 	if GameManager.enemy_type == "Wormy":
@@ -18,10 +17,10 @@ func add_extra_point():
 		addscore += 11
 	elif GameManager.enemy_type == "Crocodile":
 		addscore += 15
-	#score_label.text = "Score: " + str(score) + " / 10" + " + " + str(addscore)
 
 func _process(delta): #TOTAL POINTS
 	totalscore = score + addscore
 	GameManager.current_level_score = totalscore
 	
-	#total_score_label.text = "POINTS:" + str(totalscore).pad_zeros(2)
+	score_label.text = str(score).pad_zeros(2) + "/10"
+	#total_score_label.text = "POINTS:" + str(totalscore).pad_zeros(3)

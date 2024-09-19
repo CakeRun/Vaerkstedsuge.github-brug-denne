1c5a7c41f4efdf0167bf4d420f609f6364178375
violaaaR <viola.rosendahl@gmail.com>
2024-09-19 12:11:06 +0200

    ting og portals og end fungerer (Mangler total score til sidst)

--- GoDot/global.gd
+++ GoDot/global.gd
@@ -11,3 +11,7 @@ var total_score = 0
 var current_level_score = 0
 
 var levels_completed = 0
+
+@onready var total_score_label_end_small = $TotalScoreLabel_end_small
+@onready var total_score_label_end_medium = $TotalScoreLabel_end_medium
+@onready var total_score_label_end_big = $TotalScoreLabel_end_big

e040f99f149569a5fdad2a7f8e106cf80a869ba3
violaaaR <viola.rosendahl@gmail.com>
2024-10-02 14:25:20 +0200

    heart code things done

--- GoDot/heart.gd
+++ GoDot/heart.gd
@@ -4,6 +4,6 @@ extends Area2D
 func _on_body_entered(body):
 	if body.has_method("player"):
 		GameManager.player_heal = true
-		await get_tree().create_timer(0.02).timeout
+		await get_tree().create_timer(0.01).timeout
 		GameManager.player_heal = false
 		self.queue_free()

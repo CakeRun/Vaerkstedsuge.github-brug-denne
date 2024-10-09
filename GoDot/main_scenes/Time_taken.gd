b5a23e0a09b78dbd76c0b9dc7f8ee684e26dc197
Manu-Boss <bansal.manasvi21@gmail.com>
2024-10-09 15:24:46 +0200

    lille ting

--- GoDot/main_scenes/Time_taken.gd
+++ GoDot/main_scenes/Time_taken.gd
@@ -5,5 +5,5 @@ extends Label
 var total_time = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	total_time = int(GameManager.total_time) / 60
-	label_timer.text = "TIMER: " + str(total_time).pad_zeros(3) + " minutes"
+	total_time = int(GameManager.total_time)
+	label_timer.text = "TIMER: " + str(total_time).pad_zeros(3) + " seconds"

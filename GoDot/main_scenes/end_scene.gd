2795827fdd68efe129d42994fd97c2885ea358d8
violaaaR <viola.rosendahl@gmail.com>
2024-10-09 07:14:38 +0200

    burned cupcake

--- GoDot/main_scenes/end_scene.gd
+++ GoDot/main_scenes/end_scene.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 @onready var celebrations = $AudioStreamPlayer2D
-
+@onready var sad = $sad
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	celebrations.play()
+	if GameManager.total_score > 30:
+		celebrations.play()
+	else:
+		sad.play()

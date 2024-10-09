7f531c3515d8b7fe2e2c657a540c8cb6eb68ff39
Manu-Boss <bansal.manasvi21@gmail.com>
2024-10-09 15:16:06 +0200

    Small changes

--- GoDot/main_scenes/Time_taken.gd
+++ GoDot/main_scenes/Time_taken.gd
@@ -6,4 +6,4 @@ var total_time = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	total_time = int(GameManager.total_time) / 60
-	label_timer.text = "TIMER: " + str(total_time).pad_zeros(3) + "minutes"
+	label_timer.text = "TIMER: " + str(total_time).pad_zeros(3) + " minutes"

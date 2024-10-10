679c6a0ec065b1767740316810a552fcabdc3557
violaaaR <viola.rosendahl@gmail.com>
2024-10-10 12:09:40 +0200

    timer!!

--- GoDot/main_scenes/Time_taken.gd
+++ GoDot/main_scenes/Time_taken.gd
@@ -4,6 +4,16 @@ extends Label
 
 var total_time = 0
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
 	total_time = int(GameManager.total_time)
 	label_timer.text = "TIMER: " + str(total_time).pad_zeros(3) + " seconds"
+	
+func _time():
+	var minute = floor(total_time / 60)
+	var second = int(total_time) % 60
+	return [minute, second]
+
+func _process(_delta):
+	label_timer.text = "%02d:%02d" % _time()
+	
